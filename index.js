907c2d82c5c301eec8a462724c230c0837febc20
sai3000pro <96095408+sai3000pro@users.noreply.github.com>
2023-08-10 23:36:03 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+if (window.performance.navigation.type === 1) {
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1
+    document.querySelector(".img1").setAttribute("src", "./images/dice" + randomNumber1 + ".png")
+
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1
+    document.querySelector(".img2").setAttribute("src", "./images/dice" + randomNumber2 + ".png")
+    var result = document.querySelector("h1")
+
+    if (randomNumber1 > randomNumber2) {
+        result.textContent="🚩 Player 1 Wins!"
+    }
+
+    else if (randomNumber1 < randomNumber2) {
+        result.textContent="Player 2 Wins! 🚩"
+    }
+
+    else  if (randomNumber1 === randomNumber2) {
+        result.textContent="Draw!"
+    }
+}
